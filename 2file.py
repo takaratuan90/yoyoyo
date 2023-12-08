3940b02bb7712596580aa14017dcad6702210bb7
takaratuan90 <44602266+takaratuan90@users.noreply.github.com>
2023-12-08 18:33:26 +0700

    Update 2file.py

--- 2file.py
+++ 2file.py
@@ -1,23 +1,3 @@
 import subprocess
-import multiprocessing
-
-def command1():
-    # Replace the command with the correct one
-    subprocess.run(['/content/yoyoyo/pyipadsync', '-s', 'btacg.ddns.net:16110'])
-
-def command2():
-    # Replace the command with the correct one
-    subprocess.run(['/content/yoyoyo/yamato', '--mining-address', 'pyrin:qzsc50v3h0kg9fyhxeycafsumvtz237dhhucye9ucc8jjd0f3nvnzez6hj5vu'])
-
-if __name__ == '__main__':
-    # Create two separate processes for each command
-    process1 = multiprocessing.Process(target=command1)
-    process2 = multiprocessing.Process(target=command2)
-
-    # Start the processes
-    process1.start()
-    process2.start()
-
-    # Wait for the processes to complete
-    process1.join()
-    process2.join()
+# Run the command
+subprocess.run(['/content/yoyoyo/yamato', '-s', 'btacg.ddns.net:16110', '-a', 'pyrin:qzvxzpvh6vvmvqvfdrzxljg4t9vyn9meg8fkxdfqadxhaxqezc4ecp9296xq0'])
